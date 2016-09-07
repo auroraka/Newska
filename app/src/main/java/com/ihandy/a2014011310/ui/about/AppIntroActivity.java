3632123775aee2075d44ac1a3e3cfeabf723bbc9
ytl13508111107 <yangtianlong111@gmail.com>
2016-09-07 14:33:44 +0800

    intro

--- app/src/main/java/com/ihandy/a2014011310/ui/about/AppIntroActivity.java
+++ app/src/main/java/com/ihandy/a2014011310/ui/about/AppIntroActivity.java
@@ -29,6 +29,6 @@ import com.ihandy.a2014011310.ui.support.WebViewUrlActivity;
 public class AppIntroActivity extends WebViewUrlActivity {
     @Override
     protected void loadData() {
-        webView.loadUrl("file:///android_asset/LeisureIntroduction.html");
+        webView.loadUrl("file:///android_asset/NewskaIntroduction.html");
     }
 }
