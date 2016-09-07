62d9cdcd6a91433671892e730cb81b626e895d40
ytl13508111107 <yangtianlong111@gmail.com>
2016-09-07 14:19:12 +0800

    modify about

--- app/src/main/java/com/ihandy/a2014011310/ui/about/AboutFragment.java
+++ app/src/main/java/com/ihandy/a2014011310/ui/about/AboutFragment.java
@@ -47,22 +47,10 @@ import java.io.IOException;
 public class AboutFragment extends PreferenceFragment implements Preference.OnPreferenceClickListener{
 
     private Preference mAppIntro;
-    private Preference mDemoVideo;
-    private Preference mCheckUpdate;
-    private Preference mStarProject;
-    private Preference mShare;
-    private Preference mBlog;
-    private Preference mGitHub;
     private Preference mEmail;
 
 
     private final String APP_INTRO = "app_intro";
-    private final String DEMO_VIDEO = "demo_video";
-    private final String CHECK_UPDATE = "check_update";
-    private final String START_PROJECT = "star_project";
-    private final String SHARE = "share";
-    private final String BLOG = "blog";
-    private final String GITHUB = "github";
     private final String EMAIL = "email";
 
 
@@ -73,21 +61,9 @@ public class AboutFragment extends PreferenceFragment implements Preference.OnPr
         addPreferencesFromResource(R.xml.about);
 
         mAppIntro = findPreference(APP_INTRO);
-        mDemoVideo = findPreference(DEMO_VIDEO);
-        mCheckUpdate = findPreference(CHECK_UPDATE);
-        mStarProject = findPreference(START_PROJECT);
-        mShare = findPreference(SHARE);
-        mBlog = findPreference(BLOG);
-        mGitHub = findPreference(GITHUB);
         mEmail = findPreference(EMAIL);
 
         mAppIntro.setOnPreferenceClickListener(this);
-        mDemoVideo.setOnPreferenceClickListener(this);
-        mCheckUpdate.setOnPreferenceClickListener(this);
-        mStarProject.setOnPreferenceClickListener(this);
-        mShare.setOnPreferenceClickListener(this);
-        mBlog.setOnPreferenceClickListener(this);
-        mGitHub.setOnPreferenceClickListener(this);
         mEmail.setOnPreferenceClickListener(this);
 
         progressBar = (ProgressBar) getActivity().findViewById(R.id.progressbar);
@@ -100,47 +76,6 @@ public class AboutFragment extends PreferenceFragment implements Preference.OnPr
         if(mAppIntro == preference){
             Intent toIntro = new Intent(getActivity(),AppIntroActivity.class);
             startActivity(toIntro);
-        }else if(mDemoVideo == preference){
-            Intent toVideo = new Intent(getActivity(),DemoVideoActivity.class);
-            startActivity(toVideo);
-        }else if(mCheckUpdate == preference){
-            progressBar.setVisibility(View.VISIBLE);
-
-            Request.Builder builder = new Request.Builder();
-            builder.url(CONSTANT.VERSION_URL);
-            Request request = builder.build();
-            HttpUtil.enqueue(request, new Callback() {
-                @Override
-                public void onFailure(Request request, IOException e) {
-                    Snackbar.make(getView(), R.string.hint_fail_check_update, Snackbar.LENGTH_SHORT).show();
-                    handle.sendEmptyMessage(1);
-                }
-
-                @Override
-                public void onResponse(Response response) throws IOException {
-                    String latestVersion = response.body().string();
-                    if (CONSTANT.CURRENT_VERSION.equals(latestVersion.trim())) {
-                        Snackbar.make(getView(), getString(R.string.notify_current_is_latest), Snackbar.LENGTH_SHORT).show();
-                    } else {
-                        Snackbar.make(getView(), getString(R.string.notify_find_new_version) + latestVersion, Snackbar.LENGTH_SHORT).show();
-                    }
-                    handle.sendEmptyMessage(1);
-                }
-            });
-
-        }else if(mStarProject == preference){
-            Utils.copyToClipboard(getView(), getString(R.string.project_url));
-        }else if(mShare == preference){
-            Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-            sharingIntent.setType("text/plain");
-            sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
-            sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, getString(R.string.text_share_info));
-            startActivity(Intent.createChooser(sharingIntent, getString(R.string.text_share_leisure)));
-
-        }else if(mBlog == preference){
-            Utils.copyToClipboard(getView(),getString(R.string.author_blog));
-        }else if(mGitHub == preference){
-            Utils.copyToClipboard(getView(),getString(R.string.author_github));
         }else if(mEmail == preference){
             Utils.copyToClipboard(getView(),getString(R.string.author_email));
         }
